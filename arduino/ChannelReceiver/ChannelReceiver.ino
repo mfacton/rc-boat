1bdee8947ff26d48db7e4a02a3db578dc5a7eaf5
mfacton <mfacton1@gmail.com>
2024-05-10 05:06:49 -0400

    Fix communication issue

--- arduino/ChannelReceiver/ChannelReceiver.ino
+++ arduino/ChannelReceiver/ChannelReceiver.ino
@@ -1,33 +1,50 @@
 #include <Servo.h>
 #include <SPI.h>
-#include <nRF24L01.h>
 #include <RF24.h>
+#include <nRF24L01.h>
 
-#define MAX_CHANNELS 5
+#define MAX_CHANNELS 4
 
-const int pwmPins[MAX_CHANNELS] = {3, 5, 6, 9, 10};
+const int pwmPins[MAX_CHANNELS] = {3, 5, 6, 9};
 Servo servos[MAX_CHANNELS];
 
-const byte nrfAddress[6] = "00001";
-RF24 radio(7, 8); // CE, CSN pins
+RF24 radio(7, 8, 1000000); // CE, CSN pins
 
 uint8_t data[MAX_CHANNELS];
 
 void setup() {
   for (uint8_t c = 0; c < MAX_CHANNELS; c++) {
     servos[c].attach(pwmPins[c]);
+    servos[c].write(90);
   }
+
   radio.begin();
-  radio.openReadingPipe(1, nrfAddress);
+  radio.setDataRate(RF24_250KBPS);
+  radio.setPALevel(RF24_PA_MAX);
+  radio.setChannel(40);//0-125
+  uint8_t address[5] = "00001";
+  radio.setCRCLength(RF24_CRC_16);
+  radio.setAutoAck(0);
+  radio.setPayloadSize(4);
+  radio.openReadingPipe(0, address);
   radio.startListening();
+  Serial.begin(9600);
 }
 
 void loop() {
-  if (radio.available()) {
-    radio.read(data, MAX_CHANNELS);
-    for (uint8_t c = 0; c < MAX_CHANNELS; c++) {
-      servos[c].write(data[c]);
-    }
+  uint8_t ava = radio.available();
+  if (ava) {
+    Serial.println(ava);
+    radio.read(data, 4);
+    // Serial.println(data[0]);
+    // Serial.println(data[1]);
+    // Serial.println(data[2]);
+    // Serial.println(data[3]);
   }
+  //   radio.read(data, MAX_CHANNELS);
+  //   for (uint8_t c = 0; c < MAX_CHANNELS; c++) {
+  //     servos[c].write(data[c]);
+  //   }
+  // }
   delay(1);
 }
--- arduino/TestBoat/TestBoat.ino
+++ arduino/TestBoat/TestBoat.ino
@@ -3,7 +3,11 @@
 #define MAX_CHANNELS 5
 
 #define RIGHT_PROP 0
+#define LEFT_PROP 1 //left prop inverted
 
+//bottom -> top
+#define LEFT_DEP 2 //73 -> 173
+#define RIGHT_DEP 3 //103 -> 3
 
 const int pwmPins[MAX_CHANNELS] = {3, 5, 6, 9, 10};
 Servo servos[MAX_CHANNELS];
@@ -12,15 +16,16 @@ void setup() {
   for (uint8_t c = 0; c < MAX_CHANNELS; c++) {
     servos[c].attach(pwmPins[c]);
   }
+  servos[LEFT_PROP].write(0);
+  servos[RIGHT_PROP].write(180);
+  servos[LEFT_DEP].write(73);
+  servos[RIGHT_DEP].write(103);
+  delay(3000);
+  servos[LEFT_DEP].write(174);
+  servos[RIGHT_DEP].write(4);
+  servos[LEFT_PROP].write(90);
+  servos[RIGHT_PROP].write(90);
 }
 
 void loop() {
-  servos[1].write(80);
-  delay(1000);
-  servos[1].write(100);
-  delay(1000);
-  // servos[1].write(90);
-  // servos[2].write(90);
-  // servos[3].write(90);
-  // servos[4].write(90);
 }
